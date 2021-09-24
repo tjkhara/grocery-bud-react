57937949e80bb73a26ba71644aba6d87d7cb1e4c
Tajeshwar <tkhara@gmail.com>
2021-09-24 19:51:22 +0530

    Before remove item

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react'
+import List from './List'
+import Alert from './Alert'
+
+function App() {
+  const [name, setName] = useState('')
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
+  const [editID, setEditID] = useState(null)
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: '',
+    type: '',
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!name) {
+      // display alert
+      showAlert(true, 'danger', 'Please enter a value')
+    } else if (name && isEditing) {
+      // deal with edit
+    }
+
+    // add item to the list
+    else {
+      // show alert
+      showAlert(true, 'success', 'Item added to the list')
+      const newItem = { id: new Date().getTime().toString(), title: name }
+      setList([...list, newItem])
+      setName('')
+    }
+  }
+
+  const showAlert = (show = false, type = '', msg = '') => {
+    setAlert({
+      show,
+      msg,
+      type,
+    })
+  }
+
+  const handleClearList = () => {
+    showAlert(true, 'danger', 'All items have been deleted.')
+    setList([])
+  }
+
+  return (
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
+        <h3>Grocery Bud</h3>
+        <div className='form-control'>
+          <input
+            type='text'
+            className='grocery'
+            placeholder='e.g. eggs'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type='submit' className='submit-btn'>
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className='grocery-container'>
+          <List items={list} />
+          <button className='clear-btn' onClick={handleClearList}>
+            clear items
+          </button>
+        </div>
+      )}
+    </section>
+  )
+}
+
+export default App
